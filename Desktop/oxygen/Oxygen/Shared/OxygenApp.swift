aaa8cca0dd47eee7354ee78d86a774be11d0c13d
KarKar100 <morkiral@gmail.com>
2021-06-18 13:10:01 +0300

    Initial project version

---
+++ Desktop/oxygen/Oxygen/Shared/OxygenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxygenApp.swift
+//  Shared
+//
+//  Created by Diana on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct OxygenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
